d4a722c971b3bab55839d84675344afd8d63b4c6
user <user@1429-S119>
2019-10-25 08:20:39 +0200

    Init

---
+++ KundenLoginJacky/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KundenLoginJacky
+{
+    class Kunde
+    {
+        private String username;
+        private String password;
+        private String email;
+
+        public Kunde()
+        {
+
+        }
+        public Kunde(String userName, String passWord, String email)
+        {
+            this.username = userName;
+            this.password = passWord;
+        }
+    }
+}
