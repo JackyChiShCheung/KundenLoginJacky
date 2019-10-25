0235f6656d8a09273dba1c9fb1ed89f791ddee31
user <user@1429-S119>
2019-10-25 09:23:26 +0200

    User Eintragen und User Liste ausgeben

---
+++ KundenLoginJacky/KundenLoginTUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KundenLoginJacky;
+
+namespace KundenLoginJacky
+{
+    class KundenLoginTUI
+    {
+        
+
+        public static void Main(String[]args)
+        {
+           
+
+            List<Kunde> kundenListe = new List<Kunde>();
+
+            String userNameInp, passwordInp, emailInp;
+
+            char wdhderSchleife = ' ';
+            do {
+                Console.WriteLine("Bitte geben Sie ihren Username ein : ");
+                userNameInp = Console.ReadLine();
+                Console.WriteLine("Bitte geben Sie ihren Password ein : ");
+                passwordInp = Console.ReadLine();
+                Console.WriteLine("Bitte geben Sie ihren Email ein : ");
+                emailInp = Console.ReadLine();
+
+                Kunde neuerKunde = new Kunde(userNameInp,passwordInp,emailInp);
+
+                kundenListe.Add(neuerKunde);
+
+                Console.WriteLine("Noch eine neuen Kunden hinzufügen ? ");
+
+                wdhderSchleife = Console.ReadKey().KeyChar;
+            } while (wdhderSchleife != 'n' && wdhderSchleife!='N');
+            //TODO in Methode packen Ausgabe KundenListe
+            foreach (Kunde kund in kundenListe) { Console.WriteLine(kund.ToString()); }
+            kundenListe.Find(x => x.Username.Equals(userNameInp));
+        }
+
+    }
+}
--- KundenLoginJacky/Program.cs
+++ KundenLoginJacky/Program.cs
@@ -16,6 +16,30 @@ namespace KundenLoginJacky
         {
             this.username = userName;
             this.password = passWord;
+            this.email = email;
+        }
+
+        public String Username
+        {
+            get { return this.username; }
+            set { this.username = value; }
+        }
+
+        public String Password
+        {
+            get { return this.password; }
+            set { this.password = value; }
+        }
+
+        public String Email
+        {
+            get { return this.email; }
+            set { this.email = value ; }
+        }
+
+        public override string ToString()
+        {
+            return "Username : " + username + " Password : "+ password + " Email : " + email;
         }
     }
 }
